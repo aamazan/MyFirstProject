7710de3fb386026da49df5850296ac28243a67c4
apamazan <armaan.p.amazan@gmail.com>
2020-05-18 12:27:40 -0700

    initial commit

---
+++ MyFirstProject/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+// Refresher program to cover basic types, operations, etc.
+
+int main() {
+
+	int num = -1;
+	while (num != 0) {
+		std::cout << "Please select an option:" << std::endl;
+		std::cout << "1: N numbers of Fibonacci Sequence" << std::endl;
+		std::cout << "2: " << std::endl;
+		std::cout << "3: " << std::endl;
+		std::cout << "4: " << std::endl;
+		std::cout << "0: Quit" << std::endl;
+
+	}
+	_getch();
+	return 0;
+}
