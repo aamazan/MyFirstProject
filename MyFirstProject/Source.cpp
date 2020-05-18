9e5546c354512447f4621a9d09390874b7a902b9
apamazan <armaan.p.amazan@gmail.com>
2020-05-18 16:13:55 -0700

    updated namespace, removed std:: extensions. attempted to include fib.cpp to encapsulate function, but encountering error.

--- MyFirstProject/Source.cpp
+++ MyFirstProject/Source.cpp
@@ -1,49 +1,21 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
-
-// Refresher program to cover functions, operations, pointers, classes, etc.
-
-
-
-void fib() {
-	int n = -1;
-	int a = 0; int b = 1; int temp;
-	std::cout << "Please enter how many values of the Fibonacci Sequence you wish to print." << std::endl;
-	std::cout << "Enter 0 if you wish to exit back to the main menu." << std::endl;
-	std::cin >> n;
-	if (n > 0) {
-		for (int i = 0; i < n; i++) {
-			std::cout << a << " ";
-			temp = a + b; // store sum in temp variable
-			a = b; // shift a forward in sequence
-			b = temp; // shift b forward in sequence
-		}
-		std::cout<< "" <<std::endl;
-		std::cout << "Operation completed. Returning to main menu." << std::endl;
-		std::cout << "" << std::endl;
-	}
-	else if (n == 0) {
-		return;
-	}
-	else {
-		std::cout << "Incorrect input. Please input a positive integer or 0." << std::endl;
-		fib();
-	}
-	return;
-}
+#include "fib.cpp"
+using namespace std;
+// Refresher program to cover functions, operations, pointers, classes, etc
 
 void main() {
 
 	int num = -1;
 	while (num != 0) {
-		std::cout << "Please select an option:" << std::endl;
-		std::cout << "1: Print N numbers of Fibonacci Sequence" << std::endl;
-		std::cout << "2: Add movies to a LinkedList" << std::endl;
-		std::cout << "3: Sort movies" << std::endl;
-		std::cout << "4: Delete movies" << std::endl;
-		std::cout << "0: Quit" << std::endl;
-		std::cin >> num;
+		cout << "Please select an option:" << endl;
+		cout << "1: Print N numbers of Fibonacci Sequence" << endl;
+		cout << "2: Add movies to a LinkedList" << endl;
+		cout << "3: Sort movies" << endl;
+		cout << "4: Delete movies" << endl;
+		cout << "0: Quit" << endl;
+		cin >> num;
 		switch (num) {
 		case 0: break;
 		case 1: fib(); break;
@@ -51,7 +23,7 @@ void main() {
 		case 3: break;
 		case 4: break;
 		default:
-			std::cout << "Error. Please enter one of the options given." << std::endl;
+			cout << "Error. Please enter one of the options given." << endl;
 			break;
 		}
 	}
---
+++ MyFirstProject/fib.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// Fibonacci Sequence print function
+
+void fib() {
+	int n = -1;
+	int a = 0; int b = 1; int temp;
+	cout << "Please enter how many values of the Fibonacci Sequence you wish to print." << endl;
+	cout << "Enter 0 if you wish to exit back to the main menu." << endl;
+	cin >> n;
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
+			cout << a << " ";
+			temp = a + b; // store sum in temp variable
+			a = b; // shift a forward in sequence
+			b = temp; // shift b forward in sequence
+		}
+		cout << "" << endl;
+		cout << "Operation completed. Returning to main menu." << endl;
+		cout << "" << endl;
+	}
+	else if (n == 0) {
+		return;
+	}
+	else {
+		cout << "Incorrect input. Please input a positive integer or 0." << endl;
+		fib();
+	}
+	return;
+}
