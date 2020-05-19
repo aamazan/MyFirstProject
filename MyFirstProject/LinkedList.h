c1d9d7326d230eedf79f86153d3752d4ee0e50f6
apamazan <armaan.p.amazan@gmail.com>
2020-05-19 13:08:22 -0700

    trying to further refine the linked list cpp and header files for encapsulation purposes, but running into scope errors regarding head/tail of nodes

--- MyFirstProject/LinkedList.h
+++ MyFirstProject/LinkedList.h
@@ -1,2 +1,23 @@
 #include <iostream>
 #include <string>
+
+struct node {
+	struct node *next;
+	struct node *prev;
+	string name;
+	int rating;
+};
+
+class LinkedList {
+private:
+	node *head, *tail;
+public:
+	LinkedList() {
+		head = NULL;
+		tail = NULL;
+	}
+};
+
+void add_node(string m, int r);
+void delete_node(string m);
+void print_movies();
